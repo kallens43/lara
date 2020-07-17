08a02f1a567386cd1788f84ca227a58dfdc0dce2
kallens43 <68415887+kallens43@users.noreply.github.com>
2020-07-17 11:12:20 +0400

    Add files via upload

---
+++ resources/views/index.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>larareact</title>
+        <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+    </head>
+    <body>
+        <h1>POMOGITE</h1>
+        <button onclick "testAjax()"> test ajax</button>
+    </body>
+    <script>
+        function testAjax()
+        {
+            axios.get('/test')
+  .then(function (response) {
+    console.log(response.data);
+    })
+    .catch(function (error) {
+        alert('ERROR');
+  })
+        }
+    </script>
+</html>
